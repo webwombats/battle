dcfcfa3a7bf61befaf23da7e74c7e56d4ef4e144
Dmitriy Yakovlev <yakovlev.dragomir@gmail.com>
2020-07-11 23:22:55 +0200

    Style battle list

--- components/BattleList.tsx
+++ components/BattleList.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 
 import ErrorMessage from "./ErrorMessage";
+import CommentIcon from "./Icons/Comment";
+import UpIcon from "./Icons/Up";
 
 const BattleList = ({ data }) => {
   // const loadMorePosts = () => {
@@ -22,27 +24,44 @@ const BattleList = ({ data }) => {
 
   return (
     <section>
-      <ul>
-        {data.map((battle, index) => (
-          <li key={battle.id} className="py-4">
-            <div>
-              <p>
-                <b>Battle description</b>:
-              </p>
-              <div dangerouslySetInnerHTML={{ __html: battle.description }} />
+      {data.map((battle, index) => (
+        <Link href="/battle/[id]" as={`/battle/${battle.id}`} key={battle.id}>
+          <a className="bl-container flex mb-8 p-6 sm:p-8 bg-shark rounded-xl">
+            <div className="flex flex-col flex-grow mr-4 sm:mr-8">
+              <div className="text-base sm:text-xl md:text-2xl text-gray-400 font-bold mb-4">
+                <span className="text-havelock-blue">
+                  Sed ut perspiciatis (57%)
+                </span>{" "}
+                or{" "}
+                <span className="text-fruit-salad">
+                  Ut enim ad minima quis nostrum (43%)
+                </span>
+              </div>
+              <div className="flex flex-col sm:flex-row sm:items-center">
+                <a
+                  className="flex items-center mb-2 sm:mb-0 sm:mr-10"
+                  id={battle.userId}
+                >
+                  <span className="w-5 h-5 rounded-full bg-yellow-500 mr-2"></span>
+                  <span className="text-gray-500 text-sm sm:text-base">
+                    John Doe
+                  </span>
+                </a>
+                <div className="flex items-center">
+                  <CommentIcon className="mr-2 text-gray-500 w-5" />
+                  <span className="text-gray-500 text-sm sm:text-base">
+                    9 comment(s){" "}
+                  </span>
+                </div>
+              </div>
             </div>
-            <p>
-              <b>UserId created this battle</b>: {battle.userId}
-            </p>
-
-            <Link href="/battle/[id]" as={`/battle/${battle.id}`}>
-              <a className="underline hover:no-underline text-white">
-                Go to battle
-              </a>
-            </Link>
-          </li>
-        ))}
-      </ul>
+            <a className="bl-votes flex flex-col flex-shrink-0 items-center py-1 sm:py-2 px-3 sm:px-4 pb-2 h-16 sm:h-20 bg-charade text-gray-400 text-sm sm:text-xl font-bold rounded-xl border-2 border-gray-800">
+              <UpIcon className="text-gray-500 w-4 sm:w-5" />
+              <span>14</span>
+            </a>
+          </a>
+        </Link>
+      ))}
       {/* 
         {areMorePosts && (
           <button onClick={() => loadMorePosts()} disabled={loadingMorePosts}>
---
+++ components/Icons/Up.tsx
@@ -0,0 +1,19 @@
+const UpIcon = (props) => {
+  return (
+    <svg
+      aria-hidden="true"
+      data-prefix="fas"
+      data-icon="caret-up"
+      className="prefix__svg-inline--fa prefix__fa-caret-up prefix__fa-w-10"
+      viewBox="0 0 320 512"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M288.662 352H31.338c-17.818 0-26.741-21.543-14.142-34.142l128.662-128.662c7.81-7.81 20.474-7.81 28.284 0l128.662 128.662c12.6 12.599 3.676 34.142-14.142 34.142z"
+      />
+    </svg>
+  );
+};
+
+export default UpIcon;
